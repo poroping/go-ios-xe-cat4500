6dd8c9611e9e10dc64c882b5c5c4f94fbaaae94b
poroping <poroping@poroping.com>
2021-03-29 11:44:48 +0000

    models

--- models.go
+++ models.go
@@ -1,18 +1,1 @@
 package main
-
-type L3Vlan struct {
-	NedVlan struct {
-		Name        int    `json:"name"`
-		Description string `json:"description"`
-		IP          struct {
-			Vrf struct {
-			} `json:"vrf"`
-			Address struct {
-				Primary struct {
-					Address string `json:"address"`
-					Mask    string `json:"mask"`
-				} `json:"primary"`
-			} `json:"address"`
-		} `json:"ip"`
-	} `json:"ned:Vlan"`
-}
